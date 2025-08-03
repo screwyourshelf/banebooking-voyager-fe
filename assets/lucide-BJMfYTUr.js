224567b78d52a22b70d22ecac054f71adb9e80f5
Andreas Lotarev <andreas.lotarev@outlook.com>
2025-08-03 16:51:06 +0200

    Updates

--- assets/lucide-BJMfYTUr.js
+++ assets/lucide-BJMfYTUr.js
@@ -1,4 +1,4 @@
-import{r as c}from"./shadcn-C7L97JSy.js";/**
+import{r as c}from"./shadcn-CWEYZx5G.js";/**
  * @license lucide-react v0.513.0 - ISC
  *
  * This source code is licensed under the ISC license.
