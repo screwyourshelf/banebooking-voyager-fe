a7c3dc4ff0c72ed8cea581659eb0a761fe2f218e
Andreas Lotarev <andreas.lotarev@outlook.com>
2026-02-01 22:03:12 +0100

    Updates

---
+++ assets/Page-DY-nzD88.js
@@ -0,0 +1,1 @@
+import{j as e}from"./shadcn-ciDZoPHa.js";import{f as n}from"./index-DwBi739w.js";const o={md:"max-w-screen-md",lg:"max-w-screen-lg",full:"max-w-none"};function x({children:m,className:s,width:a="md"}){return e.jsx("div",{className:n(o[a],"mx-auto px-1 py-2",s),children:m})}export{x as P};
--- assets/lucide-DUUYvuJp.js
+++ assets/lucide-DUUYvuJp.js
@@ -1,4 +1,4 @@
-import{r as c}from"./shadcn-mOdBhBu9.js";/**
+import{r as c}from"./shadcn-ciDZoPHa.js";/**
  * @license lucide-react v0.542.0 - ISC
  *
  * This source code is licensed under the ISC license.
