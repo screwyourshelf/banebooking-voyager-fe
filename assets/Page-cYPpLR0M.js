9f607755f75535888f1f885ad962fbca50447ed1
Andreas Lotarev <andreas.lotarev@outlook.com>
2026-03-02 19:40:40 +0100

    Updates

---
+++ assets/Page-cYPpLR0M.js
@@ -0,0 +1,1 @@
+import{j as e}from"./react-CWoCI9K4.js";import{l as n}from"./index-CS36PtSo.js";const o={md:"max-w-screen-md",lg:"max-w-screen-lg",full:"max-w-none"};function x({children:m,className:s,width:a="md"}){return e.jsx("div",{className:n(o[a],"mx-auto px-1 py-2",s),children:m})}export{x as P};
