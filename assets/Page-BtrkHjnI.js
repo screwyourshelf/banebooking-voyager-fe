5f01a07bd266814888cfdafd5e05e608e29bc697
Andreas Lotarev <andreas.lotarev@outlook.com>
2026-03-02 19:05:38 +0100

    Updates

--- assets/Page-BtrkHjnI.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./react-CWoCI9K4.js";import{j as n}from"./index-DpQ2owuz.js";const o={md:"max-w-screen-md",lg:"max-w-screen-lg",full:"max-w-none"};function x({children:m,className:s,width:a="md"}){return e.jsx("div",{className:n(o[a],"mx-auto px-1 py-2",s),children:m})}export{x as P};
