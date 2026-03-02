a36484de46671cf55fd036d8529808902343b5da
Andreas Lotarev <andreas.lotarev@outlook.com>
2026-03-02 12:58:16 +0100

    Updates

--- assets/AccordionActions-DzKbAmoX.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./react-CWoCI9K4.js";import{j as s}from"./index-CpSDTCJS.js";function e({children:o,className:r}){return t.jsx("div",{className:s("flex justify-end pt-2 border-t",r),children:o})}export{e as A};
