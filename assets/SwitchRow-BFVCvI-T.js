3bba50e8d720e6418ff3101f2a0cfe8caf7dfeaf
Andreas Lotarev <andreas.lotarev@outlook.com>
2026-03-05 16:12:27 +0100

    Updates

---
+++ assets/SwitchRow-BFVCvI-T.js
@@ -0,0 +1,1 @@
+import{j as o}from"./react-6UFIRJaT.js";import{R as c}from"./RowList-k9rhBPQF.js";import{S as f}from"./switch-BNVpajSn.js";function n({title:t,description:r,checked:s,onCheckedChange:i,disabled:m=!1,className:a,density:p="compact"}){return o.jsx(c,{title:t,description:r,density:p,className:a,right:o.jsx(f,{checked:s,onCheckedChange:i,disabled:m})})}export{n as S};
---
+++ assets/chevron-down-DOqGB2Ix.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-BOIbq3K8.js";const n=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],c=o("chevron-down",n);export{c as C};
