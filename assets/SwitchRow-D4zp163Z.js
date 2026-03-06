b593f4d752b258a1957c2ad23b8738b7344f053f
Andreas Lotarev <andreas.lotarev@outlook.com>
2026-03-06 16:33:38 +0100

    Updates

--- assets/SwitchRow-D4zp163Z.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./react-CvlzC2Xd.js";import{R as c}from"./RowList-CSpgNaJL.js";import{S as f}from"./switch-ob6uJFK-.js";function n({title:t,description:r,checked:s,onCheckedChange:i,disabled:m=!1,className:a,density:p="compact"}){return o.jsx(c,{title:t,description:r,density:p,className:a,right:o.jsx(f,{checked:s,onCheckedChange:i,disabled:m})})}export{n as S};
