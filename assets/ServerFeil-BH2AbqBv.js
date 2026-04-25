f372ea77491487b56a8bf5ae794038928b7d6071
Andreas Lotarev <andreas.lotarev@outlook.com>
2026-04-25 17:56:13 +0200

    Updates

--- assets/ServerFeil-BH2AbqBv.js
+++
@@ -1,1 +0,0 @@
-import{j as r,c1 as s}from"./react-COiYNNZu.js";import{A as t,o as i}from"./index-C65WBptT.js";function l({feil:e}){return e?r.jsxs(t,{variant:"destructive",children:[r.jsx(s,{className:"h-4 w-4"}),r.jsx(i,{children:e})]}):null}export{l as S};
--- assets/SwitchRow-D2QMb4zI.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./react-COiYNNZu.js";import{R as c}from"./RowList-D3W6zbBu.js";import{S as f}from"./switch-BAaqrWzm.js";function n({title:t,description:r,checked:s,onCheckedChange:i,disabled:m=!1,className:a,density:p="compact"}){return o.jsx(c,{title:t,description:r,density:p,className:a,right:o.jsx(f,{checked:s,onCheckedChange:i,disabled:m})})}export{n as S};
